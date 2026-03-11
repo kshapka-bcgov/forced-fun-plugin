f3e52c7aa43aff4440912faaa532c2f15ce98b7b
kshapka-bcgov <kyle.shapka@gov.bc.ca>
2026-03-11 10:53:58 -0700

    plugin scaffold

---
+++ build/forced-fun-plugin/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '43de2d4f9396fa4fdb20');
---
+++ build/forced-fun-plugin/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'e02237179cf21f1923d2');
